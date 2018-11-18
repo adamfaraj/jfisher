1a3797f59a0de6f83ea9d0f847688396feaa8123
adamfaraj <faraj.9798@gmail.com>
2018-11-17 20:44:00 -0500

    animate on scroll

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-import scrollToComponent from 'react-scroll-to-component';
 import './Navigation.css';
 
 class Navigation extends React.Component {
+        constructor(props) {
+            super(props);
+        }
 
-    componentDidMount() {
-        // scrollToComponent()
+    toHome = (e) => {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+    toAbout = () => {
+        const about = document.getElementById('about');
+        about.scrollIntoView({behavior: 'smooth'});
+    }
+
+    toShows = () => {
+        const shows = document.getElementById('shows');
+        shows.scrollIntoView({behavior: 'smooth'});
+    }
+    toVideos = () => {
+        const videos = document.getElementById('videos');
+        videos.scrollIntoView({behavior: 'smooth'});
+    }
+    toPhotos = () => {
+        const photos = document.getElementById('photos');
+        photos.scrollIntoView({behavior: 'smooth'});
+    }
+    toContact = () => {
+        const contact = document.getElementById('contact');
+        contact.scrollIntoView({behavior: 'smooth'});
     }
 
     render() {
@@ -16,32 +43,32 @@ class Navigation extends React.Component {
             <Navbar inverse collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="#home">Jourdain Fisher</a>
+                        <a href="" onClick={this.toHome}>Jourdain Fisher</a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav pullRight>
-                        <NavItem eventKey={1} href="#about">
+                        <NavItem eventKey={1} href="" onClick={this.toAbout}>
                             About Jourdain
                         </NavItem>
-                        <NavItem eventKey={2} href="#shows" onClick={() => scrollToComponent(this.Shows)}>
+                        <NavItem eventKey={2} href="" onClick={this.toShows}>
                             Shows
                         </NavItem>
-                        <NavItem eventKey={3} href="#videos">
+                        <NavItem eventKey={3} href="" onClick={this.toVideos}>
                             Videos
                         </NavItem>
-                        <NavItem eventKey={4} href="#photos">
+                        <NavItem eventKey={4} href="" onClick={this.toPhotos}>
                             Photos
                         </NavItem>
-                        <NavItem eventKey={5} href="#contact">
+                        <NavItem eventKey={5} href="" onClick={this.toContact}>
                             Contact
                         </NavItem>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
         )
-    }
+    };
 }
 
 export default Navigation;
